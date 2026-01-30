c647458db19c60337e09b7a26ad3b325042eba31
suteechu <96893367+suteechu@users.noreply.github.com>
2026-01-30 15:29:25 +0700

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Card = ({ children, className = "" }) => (
-  <div className={`bg-white rounded-3xl border border-slate-100 shadow-sm hover:shadow-md transition-all duration-300 ${className}`}>
-    {children}
-  </div>
-);
